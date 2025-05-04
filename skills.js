57655558a5905fe495c1a82743a920b9223c5498
Your Name <@example.com>
2025-05-04 23:39:22 +0530

    add dyanamic skills section through js + add ew dev and prod scripts

---
+++ skills.js
@@ -0,0 +1,22 @@
+const skills =[
+    {name:"Python", Image : "./images/python.png"},
+    {name:"ANGULAR", Image : "./images/angular.png"},
+    {name:"REACT.JS", Image : "./images/react.jpg"},
+    {name:"HTML", Image : "./images/html.png"},
+    {name:"JAVASCRIPT", Image : "./images/js.jpeg"},{name:"CSS ", Image : "./images/css.png"},
+    {name:"DJANGO ", Image : "./images/django.png"},
+    {name:"NODE.js ", Image : "./images/nodejs.png"},
+    {name:"DOCKER", Image : "./images/docker.png"}
+]
+const skillSectionListId = document.getElementById('conveyor-content')
+const skillList = skills.map((i)=>{
+    return `<li class="conveyor-item m-2 text-center">
+  <img class="w-24 h-24 rounded-full mx-auto" src="${i.Image}" alt="Skill ${i.name}">
+  <p class="mt-2 text-sm font-medium">${i.name}</p>
+</li>`
+})
+
+
+skillSectionListId.innerHTML =skillList
+
+console.log(skillSectionListId);
